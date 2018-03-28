20e93fea2f431440c412c8ef799b13777a50e43a
Neil Locketz <neil@nlocketz.com>
2018-03-28 19:10:56 -0400

    Extract news URLs

--- data-collection/get_real_news_links.py
+++ data-collection/get_real_news_links.py
@@ -5,6 +5,8 @@ from enum import Enum
 import newspaper
 import requests
 import time
+import json
+import sys
 
 class BiasRating(Enum):
     Left      = 1
@@ -17,18 +19,13 @@ class BiasRating(Enum):
     @staticmethod
     def from_string(s):
         s = s.upper()
-        if s == "LEFT":
-            return BiasRating.Left
-        elif s == "LEFT-CENTER":
-            return BiasRating.LeanLeft
-        elif s == "CENTER":
-            return BiasRating.Center
-        elif s == "RIGHT-CENTER":
-            return BiasRating.LeanRight
-        elif s == "RIGHT":
-            return BiasRating.Right
-        elif s == "ALLSIDES":
-            return BiasRating.All
+        if s == "RIGHT" \
+           or s == "ALLSIDES" \
+           or s == "RIGHT-CENTER" \
+           or s == "CENTER" \
+           or s == "LEFT-CENTER" \
+           or s == "LEFT":
+            return s
         else:
             raise Exception("unkown bias: %s" % s)
 
@@ -46,25 +43,36 @@ def get_entry_urls(url):
         soup.select("td.views-field.community-feedback div div.rate-details span.disagree")
     ]
 
-    output = []
     for selector in zip(*selectors):
         bias_str = selector[1]['href']
         bias_str = bias_str[bias_str.rfind('/')+1:]
-        output.append({
+        yield {
             "source-url" : selector[0]['href'],
             "source-name" : selector[0].text,
             "source-bias" : BiasRating.from_string(bias_str),
             "source-score": (int(selector[2].text), int(selector[3].text))
-        })
-    return output
+        }
 
-def get_site_link(sub_url):
+def get_site_link(sub_url, name):
     soup = BeautifulSoup(requests.get("https://www.allsides.com/" + sub_url).text, "lxml")
-    return soup.find_all("div", {'class': 'source-image'})[0].a['href']
+    ans = soup.select("div.span4.source-image-wrapper.News.Media div.source-image a")[0]['href']
+    if ans == "http:// ":
+        for x in soup.select(
+                "article.node.node-news-source div.row-fluid.field.field-name-body.field-type-text-with-summary div.field-items p a"):
+            if name in x.text:
+                return x['href']
+    else:
+        return ans
 
 start_url = 'https://www.allsides.com/media-bias/media-bias-ratings?field_news_source_type_tid=2&field_news_bias_nid=1&title=&page='
 
 if __name__ == "__main__":
+    output = []
     for i in range(4):
         for e in get_entry_urls(start_url + str(i)):
-            print(e)
+            e['source-url'] = get_site_link(e['source-url'], e['source-name'])
+            if e['source-url'] is None:
+                continue
+            output.append(e)
+            time.sleep(0.15)
+    json.dump(output, sys.stdout)
