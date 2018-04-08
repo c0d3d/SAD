eb0107ae0d20fdad769dcb5688820c3fbf8a2214
taylorcmurphy <taylor.connell.murphy@gmail.com>
2018-04-08 15:04:59 -0400

    parse_articles now writes to a single file

--- data-collection/parse_articles.py
+++ data-collection/parse_articles.py
@@ -5,28 +5,27 @@ import sys
 import time
 
 
-def parse(url_file, real):
-    out_file = 'real.tsv' if real else 'fake.tsv'
-    with open(out_file, 'w') as out:
-        out.write('{}\t{}\t'.format('site', 'url', 'title', 'body', 'date', 'fake'))
-        reader = csv.reader(url_file, delimiter='\t')
-        for site, url in reader:
-            time.sleep(.1)
-            article = newspaper.Article(url)
-            article.download()
-            article.parse()
-            fake = 0 if real else 1
-            out.write('{}\t{}\t{}\t{}\t{}\t{}'.format(site,
-                                                      url,
-                                                      article.title,
-                                                      article.text,
-                                                      article.publish_date,
-                                                      fake))
+def parse(out, url_file, real):
+    out.write('{}\t{}\t'.format('site', 'url', 'title', 'body', 'date', 'fake'))
+    reader = csv.reader(url_file, delimiter='\t')
+    for site, url in reader:
+        time.sleep(.1)
+        article = newspaper.Article(url)
+        article.download()
+        article.parse()
+        fake = 0 if real else 1
+        out.write('{}\t{}\t{}\t{}\t{}\t{}'.format(site,
+                                                  url,
+                                                  article.title,
+                                                  article.text,
+                                                  article.publish_date,
+                                                  fake))
 
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as real:
         with open(sys.argv[2], "r") as fake:
-            parse(real, real=True)
-            parse(fake, real=False)
+            with open(sys.argv[3], "w+") as out:
+                parse(out, real, real=True)
+                parse(out, fake, real=False)
     print("Done!")
